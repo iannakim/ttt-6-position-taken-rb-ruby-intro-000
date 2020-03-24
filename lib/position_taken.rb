7d9225b05d7920b195cbc17d94fc64fb9dde427c
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-24 12:49:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 def position_taken?(board, index)
   if board.length() > 0
-    board[index].strip != ""
+    value = board[index]
+    if value != nil
+      return value.strip != ""
+    end
   else
     false
   end
